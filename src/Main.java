cca760a113e2493c9791e6ac8402175f8d1b2945
adhikari1983 <91963837+adhikari1983@users.noreply.github.com>
2023-07-08 14:10:11 -0400

    i have added sun it has some errors to be resolved

--- src/Main.java
+++ src/Main.java
@@ -7,5 +7,10 @@ public class Main {
             Omnax.Sun="1";
             Omnax.Planets="2";
             Omnax.Star="4";
+
+            FeatureSun kiranX = new FeatureSun();
+            kiranX.name="poya";
+            kiranX.temp = 12344231 celcius;
+            kiranX.size = "32942km";
     }
 }
