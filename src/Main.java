33074eafac754f4f3a1618bf8a5a7591004182ac
adhikari1983 <91963837+adhikari1983@users.noreply.github.com>
2023-07-09 10:41:44 -0400

    the feature planet1 is added and working fine

--- src/Main.java
+++ src/Main.java
@@ -12,5 +12,11 @@ public class Main {
             kiranX.name="poya";
             kiranX.temp = "12344231 celcius";
             kiranX.size = "32942km";
+
+            Planet1 xeon = new Planet1();
+            xeon.name = " reon";
+            xeon.color = "pink";
+            xeon.temp ="-45celcius";
+            xeon.size="1cm";
     }
 }
